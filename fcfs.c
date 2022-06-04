98e6e32e760456ea73deffcf286a0391738424cc
adamwolfnik <nikhilmjain1066@gmail.com>
2022-06-04 18:04:41 +0530

    Prcess Scheduling FCFS  algorithm

--- fcfs.c
+++ fcfs.c
@@ -1,42 +1,41 @@
-#include<stdio.h>
-#include<conio.h>
- int main(){
-	 int AT[10],BT[10],CT[10],WT[10],TAT[10];
-	 int temp,n,burst=0;
-	 int awt,atat;
-	 scanf("%d",&n);
-	 for(int i=0;i<n;i++){
-		 scanf("%d",&AT[i]);
-	 }
-	 for(int i=0;i<n;i++){
-		 scanf("%d",&BT[i]);
-	 }
-	 for(int i=0;i<n;i++){
-		 for(int j=i+1;j<n;j++){
-		 if(a[i]>a[j]){
-			 temp=a[i];
-			 a[i]=a[j];
-			 a[j]=temp;
-			 temp=b[i];
-			 b[i]=b[j];
-			 b[j]=temp;
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int n,j;
+	scanf("%d",&n);
+	int AT[n],BT[n],WT[n],TAT[n],cmpl_t[n],burst=0;
+	for(int i=0;i<n;i++){
+		scanf("%d",&AT[i]);
+	}
+	for(int i=0;i<n;i++){
+	scanf("%d",&BT[i]);
+	}
+	for(int i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(AT[i]>AT[j]){
+				int temp;
+				temp=AT[i];
+				AT[i]=AT[j];
+				AT[j]=temp;
+				temp=BT[i];
+				BT[i]=BT[j];
+				BT[j]=temp;
 			}
-		 }
-	 }
-	 for(int i=0;i<n;i++){
-	 if(burst<A[i]){
-		 burst++;
-	 }
-	 if(i==0){
-		 WT[i]=AT[i];
-	 }
-	 else{
-		 WT[i]=burst-AT[i];
-	 }
-	 burst+=BT[i];
-	 CT[i]=burst;
-	 }
-	 for(int i=0;i<n;i++){
-		 TAT[i]=CT[i]-BT[i];
-	 }
- }
+		}
+	}
+	for(int i=0;i<n;i++){
+		if(AT[i]>burst)
+			burst++;
+		if(i==0){
+			WT[i]=AT[i];
+		}
+		else{
+			WT[i]=burst-AT[i];
+		}
+		burst+=BT[i];
+		cmpl_t[i]=BT[i];
+	}
+	for(int i=0;i<n;i++){	
+		TAT[i]=cmpl_t[i]-AT[i];
+	}
+}
